16ec79c6e283f86d8742c445360c17c345cc80b0
mistymidnights <mistydeveloping@gmail.com>
2022-08-23 20:02:44 +0200

    fix wiggle

--- src/components/Chronometer.jsx
+++ src/components/Chronometer.jsx
@@ -20,7 +20,7 @@ const Chronometer = () => {
 
   return (
     <div className="chronometer">
-      <h2>
+      <h2 className="span">
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
